477a9fdd163b299ab3dd5a16cd2c08ff20bd8428
brunosmaciel <brunosazanamaciel@gmail.com>
2022-04-29 09:26:00 -0300

    final commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import GeraCPF from "./modules/GeraCPF";
+
 import './assets/css/style.css';
+
+function start(){
+    const gera = new GeraCPF()
+    const cpfGerado = document.querySelector('.cpf-gerado')
+    cpfGerado.innerHTML = gera.geraNovoCPF()
+    console.log('t')
+}
+start()
